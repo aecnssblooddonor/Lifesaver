e3b88cba22c04b920168833986b221c1ac9e489f
aecnssblooddonor <aeccivil2010new@gmail.com>
2026-06-04 16:04:05 +0530

    Implement donor data fetching and display function
    Fetch donor data from a JSON file and display based on selected blood group.

---
+++ app.js
@@ -0,0 +1,52 @@
+let donors=[];
+
+fetch("data/donors.json")
+.then(response=>response.json())
+.then(data=>{
+donors=data;
+});
+
+function showDonors(){
+
+let blood=document.getElementById("bloodGroup").value;
+
+let filtered=donors.filter(
+d=>d.blood===blood
+);
+
+let html="";
+
+if(filtered.length===0){
+
+html="<h3>No donors found</h3>";
+
+}else{
+
+html=`
+<table>
+<tr>
+<th>Name</th>
+<th>Department</th>
+<th>Blood Group</th>
+</tr>
+`;
+
+filtered.forEach(d=>{
+
+html+=`
+<tr>
+<td>${d.name}</td>
+<td>${d.department}</td>
+<td>${d.blood}</td>
+</tr>
+`;
+
+});
+
+html+="</table>";
+
+}
+
+document.getElementById("results").innerHTML=html;
+
+}
